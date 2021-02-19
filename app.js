4909925cb4379562ff3960b6db70797b27838ecb
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2021-02-19 14:37:04 -0500

    Revert changes

--- app.js
+++ app.js
@@ -52,14 +52,12 @@ client.on('guildCreate', guild => {
 // Denies reacting and message sending permissions for users with Muted role.
 client.on('guildMemberUpdate', (oldMember, newMember) => {
   newMember.guild.channels.cache.forEach(channel => {
-    if (newMember.roles.cache.find(role => role.name === "Muted")) {
-      if (channel.type === "text" && newMember === channel.members.find(member => member.id === newMember.id)) {
-        channel.overwritePermissions([{
-          id: newMember.guild.roles.cache.find(role => role.name === "Muted"),
-          deny: ['ADD_REACTIONS', 'SEND_MESSAGES', 'SEND_TTS_MESSAGES'] 
-        }])
-      }
-    } 
+    if (channel.type === "text" && newMember === channel.members.find(member => member.id === newMember.id)) {
+      channel.overwritePermissions([{
+        id: newMember.guild.roles.cache.find(role => role.name === "Muted"),
+        deny: ['ADD_REACTIONS', 'SEND_MESSAGES', 'SEND_TTS_MESSAGES'] 
+      }])
+    }
   });
 })
 
