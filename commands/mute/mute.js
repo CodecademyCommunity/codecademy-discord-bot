9b2076515d6f9f6543c4b32e837c2732e8541515
Steven C <stevenvcopeland@gmail.com>
2021-02-20 12:41:52 -0500

    Making some minor changes

--- commands/mute/mute.js
+++ commands/mute/mute.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
+var dateFormat = require('dateformat');
+
 module.exports = {
     name: 'mute',
     description: 'Mute a user',
@@ -5,44 +8,49 @@ module.exports = {
     execute(msg, con) {
         let toMute = undefined;
         let reason = undefined;
-        let reason = undefined;
-        var invalid = false;
 
         if (!msg.member.roles.cache.some(
         role => role.name === "Moderator" || role.name === "Admin")) {
-            msg.reply("You must be a moderator or admin to use this command.");
-            invalid = true;
+            return msg.reply("You must be a moderator or admin to use this command.");
         } else {
             toMute = msg.mentions.members.first();
             if (!toMute) {
-                msg.reply("Please provide a user to mute.");
-                invalid = true;
+                return msg.reply("Please provide a user to mute.");
             } else if (toMute === msg.member) {
-                msg.reply("You cannot mute yourself.");
-                invalid = true;
+                return msg.reply("You cannot mute yourself.");
             } else if (toMute.roles.cache.some(
             role => role.name === "Moderator" || role.name === "Admin")) {
-                msg.reply("You cannot mute a moderator or admin.");
-                invalid = true;
+                return msg.reply("You cannot mute a moderator or admin.");
             } else {
                 reason = msg.content.substr(msg.content.indexOf(">") + 2);
                 if (reason === "") {
-                    msg.reply("Please provide a reason for muting.");
-                    invalid = true;
+                    return msg.reply("Please provide a reason for muting.");
                 }
             }
     
             // Adds Muted role to user.
-            if (!invalid) {
-                toMute.roles.add(msg.guild.roles.cache.find(role => role.name === "Muted"));
-                msg.channel.send(`${toMute} was muted by ${msg.member}.\nReason: ${reason}`);
-            }
+            toMute.roles.add(msg.guild.roles.cache.find(role => role.name === "Muted"));
+            msg.channel.send(`${toMute} was muted by ${msg.member}.\nReason: ${reason}`);
         }
+
+        // Outputs a message to the audit-logs channel
+        let channel = msg.guild.channels.cache.find(channel => channel.name === 'audit-logs')
+
+
+        const muteEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${toMute.user.username}#${toMute.user.discriminator} was unmuted by ${msg.author.tag}:`)
+            .setDescription(reason)
+            .setThumbnail(`https://cdn.discordapp.com/avatars/${toMute.user.id}/${toMute.user.avatar}.png`)
+            .setTimestamp()
+            .setFooter(`${msg.guild.name}`);
+
+        channel.send(muteEmbed);
 
         // Add record to infractions table.
         const timestamp = new Date(new Date().getTime() + 60 * 60 * 24 * 1000);
         var sql = `INSERT INTO infractions (timestamp, user, action, length_of_time, reason, invalid, moderator) 
-        VALUES ('${timestamp}', '${toMute}', 'cc!mute', 'N/A', '${reason}', '${invalid}', '${msg.member.displayName}')`;
+        VALUES ('${timestamp}', '${toMute}', 'cc!mute', 'N/A', '${reason}', true, '${msg.member.displayName}')`;
 
         con.query(sql, function (err, result) {
             if (err) {
--- commands/mute/unmute.js
+++ commands/mute/unmute.js
@@ -1,35 +1,47 @@
+const Discord = require('discord.js');
+var dateFormat = require('dateformat');
+
 module.exports = {
     name: 'unmute',
     description: 'Unmute a user',
 
     execute(msg, con) {
         var toUnmuteDisplay = undefined;
-        var invalid = false;
 
         if (!msg.member.roles.cache.some(
         role => role.name === "Moderator" || role.name === "Admin" || role.name === "Super User")) {
-            msg.reply("You must be a super user, moderator, or admin to use this command.");
-            invalid = true;    
+            return msg.reply("You must be a super user, moderator, or admin to use this command.");
+        } 
+
+        const toUnmute = msg.mentions.members.first();
+        if (!toUnmute) {
+            return msg.reply("Please provide a user to unmute.");
         } else {
-            const toUnmute = msg.mentions.members.first();
-            if (!toUnmute) {
-                msg.reply("Please provide a user to unmute.");
-                invalid = true;
-            } else {
-                toUnmuteDisplay = toUnmute.displayName;
-            }
+            toUnmuteDisplay = toUnmute.displayName;
         }
 
+        // Invalid is only used to specifiy whether the infraction has been "removed" or not.
+
         // Removes Muted role from user.
-        if (!invalid) {
-            toUnmute.roles.remove(msg.guild.roles.cache.find(role => role.name === "Muted"));
-            msg.channel.send(`${toUnmute} was unmuted.`);
-        }
+        toUnmute.roles.remove(msg.guild.roles.cache.find(role => role.name === "Muted"));
+        msg.channel.send(`${toUnmute} was unmuted.`);
+
+        // Outputs a message to the audit-logs channel
+        let channel = msg.guild.channels.cache.find(channel => channel.name === 'audit-logs')
+        
+        const banEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${toUnmute.user.username}#${toUnmute.user.discriminator} was unmuted by ${msg.author.tag}:`)
+            .setThumbnail(`https://cdn.discordapp.com/avatars/${toUnmute.user.id}/${toUnmute.user.avatar}.png`)
+            .setTimestamp()
+            .setFooter(`${msg.guild.name}`);
+
+        channel.send(banEmbed);
 
         // Add record to infractions table.
         const timestamp = new Date(new Date().getTime() + 60 * 60 * 24 * 1000);
         var sql = `INSERT INTO infractions (timestamp, user, action, length_of_time, reason, invalid, moderator) 
-        VALUES ('${timestamp}', '${toUnmuteDisplay}', 'cc!unmute', 'N/A', 'N/A', '${invalid}', '${msg.member.displayName}')`;
+        VALUES ('${timestamp}', '${toUnmute}', 'cc!unmute', 'N/A', 'N/A', true, '${msg.member.tag}')`;
 
         con.query(sql, function (err, result) {
             if (err) {
