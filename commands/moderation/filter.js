a1d3b66ad46dcf5296c5a4c13556ebccb3d03eba
Shaylin C <66368978+Shaylin-TRK@users.noreply.github.com>
2021-03-12 13:32:26 -0600

    Major refactoring
    Edited some of the bad words lists,
    refactored code into different functions,
    added convertToChar function to help format sentence correctly

--- commands/moderation/filter.js
+++ commands/moderation/filter.js
@@ -15,7 +15,7 @@
        penis         vagina
        asshole       sex
        sexed         piss
-       pissed        sexy
+       pissed        condom
        sexual        sexuality
        bastard       clitoris
        bitch         boobs
@@ -25,7 +25,7 @@
        fornicate     fornication
        adultery      adulter
        adulteress    slut
-       condom        shit
+               
        
     Minor Words    
        *3+ of these words will trigger the filter
@@ -35,6 +35,7 @@
        dang        heck
        frick       fricked
        crap        omg
+       shit        sexy
       
 */
 
@@ -54,55 +55,45 @@ module.exports = {
         role.name === 'Admin'
     )
   ) {
-      // list of words in users message
-      const words = msg.content.toLowerCase().split(' ');
 
-      // regex expression of strong offensive words
-      const strongWords = /hell|damn|goddamn|goddamned|damned|damnit|hell|fuck|fucked|fucking|fucker|fuckwad|fuckwad|motherfucker|wtf|nigger|nigga|penis|vagina|asshole|sex|sexed|piss|pissed|sexy|sexual|sexuality|bastard|bitch|boobs|semen|sperm|jizz|jizzed|whore|prostitute|fornicate|fornication|adultery|adulter|adulteress|slut|buttplug|clitoris|condom|shit[.,/\\:;\*\?\!\@\#\$]?/;
+      let words = convertToChar(msg.content).split(' ');
 
-      // regex expression of minor offensive words
-      const lightWords = /darn|dern|darned|derned|darnit|dernit|dang|heck|frick|fricked|crap|omg[.,/\\:;\*\?\!\@\#\$]?/;
+      console.log(words);
 
-      // numbers of strong and minor offensive words
-      let numStrongWords = 0;
-      let numLightWords = 0;
-
-      for (let i = 0; i < words.length; i++) {
-        if (strongWords.test(words[i])) {
-          numStrongWords++;
-        } else if (lightWords.test(words[i])) {
-          numLightWords++
-        }
-      }
-
-      // A single strong word or multiple light words invokes action
-      if (numStrongWords > 0 || numLightWords > 2) {
-        msg.delete();
-
-        const logs = msg.guild.channels.cache.find(channel => channel.name === 'audit-logs')
-
+      if (needsAction(words)) {
+        
+        moderation(msg);
         dmTheUser(msg);
         
-        logs.send(`I warned ${msg.author} for saying something hurtful in ${msg.channel}...`);
-        
-        // prepare to call addnote function
-        const command = `cc!addnote ${msg.author} excessive cussing/offensive language in ${msg.channel}.`;
-
-        const addition = ` User said: ${msg.content}`;
-        
-        // call addnote function based on length of user message
-        if (command.length + addition.length <= 255) {
-          logs.send(command + addition);
-        } else {
-          logs.send(command + ' Users message was to long too copy into database.');
-        }
       }
     }
   },
 };
 
+
+
+const moderation = (msg) => {
+  
+  msg.delete();
+  
+  const logs = msg.guild.channels.cache.find(channel => channel.name === 'audit-logs');
+  
+  logs.send(`I warned ${msg.author} for saying something hurtful in ${msg.channel}...`);
+        
+  const command = `cc!addnote ${msg.author} excessive cussing/offensive language in ${msg.channel}.`;
+
+  const addition = ` User said: ${msg.content}`;
+        
+  if (command.length + addition.length <= 255) {
+    logs.send(command + addition);
+  } else {
+    logs.send(command + ' Users message was to long too copy into database.');
+  }
+}
+
+
+
 const dmTheUser = (msg) => {
-  // Create an embed, craft it, and DM the user
   const Embed = new Discord.MessageEmbed()
     .setColor('#f1d302')
     .setTitle(`Reminder to ${msg.author.username}`)
@@ -111,3 +102,54 @@ const dmTheUser = (msg) => {
     .setFooter(`${msg.guild.name}`);
   msg.author.send(Embed);
 }
+
+
+
+const convertToChar = (sentence) => {
+  let result = '';
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if ((sentence[i] >= 'a' && sentence[i] <= 'z') || (sentence[i] >= 'A' && sentence[i] <= 'Z') || sentence[i] === ' ') {
+      result += sentence[i];
+    } else if (sentence[i] === '@') {
+      result += 'a';
+    } else if (sentence[i] === '1' || sentence[i] === '|' || sentence[i] === '!') {
+      result += 'i';
+    } else if (sentence[i] === '$') {
+      result += 's';
+    }
+  }
+  return result;
+}
+
+
+
+const needsAction = (words) => {
+
+  let result = false;
+
+  const strongWords = /hell|damn|goddamn|goddamned|damned|damnit|hell|fuck|fucked|fucking|fucker|fuckwad|fuckwad|motherfucker|wtf|nigger|nigga|penis|vagina|asshole|sex|sexed|piss|pissed|sexual|sexuality|bastard|bitch|boobs|semen|sperm|jizz|jizzed|whore|prostitute|fornicate|fornication|adultery|adulter|adulteress|slut|buttplug|clitoris|condom/;
+
+  const lightWords = /darn|dern|darned|derned|darnit|dernit|dang|heck|frick|fricked|crap|omg|sexy|shit/;
+
+  let numLightWords = 0;
+  let numStrongWords = 0;
+
+  let i = 0;
+  
+  while (i < words.length && !result) {
+    if (strongWords.test(words[i])) {
+      result = true;
+    } else if (lightWords.test(words[i])) {
+      numLightWords++;
+    }
+
+    if (numLightWords >= 3) {
+      result = true;
+    }
+
+    i++;
+  }
+
+  return result;
+} 
