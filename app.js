cad985c4d042b8241e10fb03990c1562fa20476d
Shaylin C <66368978+Shaylin-TRK@users.noreply.github.com>
2021-03-17 16:58:11 -0500

    added extra condition to else if
    Prevents bot from crashing in response to a DM

--- app.js
+++ app.js
@@ -195,7 +195,7 @@ const commandParser = (msg) => {
 client.on('message', (msg) => {
   if (msg.content.substring(0, 3) === 'cc!' && !(msg.member === client)) {
     commandParser(msg);
-  } else if (msg.author.id != client.user.id) {
+  } else if (msg.author.id != client.user.id && msg.guild !== null) {
     client.commands.get('filter').execute(msg);
   }
 });
