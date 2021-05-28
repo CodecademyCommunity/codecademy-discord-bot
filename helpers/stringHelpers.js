ad668ccc86dae999f3e47ad8e1ca57d7ed3ddf32
aedwardg <aedwardg1@gmail.com>
2021-05-28 15:35:13 -0700

    Update string helpers

--- helpers/stringHelpers.js
+++ helpers/stringHelpers.js
@@ -1,14 +1,21 @@
 module.exports = {
   name: 'strings',
-  isOver255(text, preText = '', postText = '') {
-    const fullText = `${preText}${text}${postText}`;
-
-    const lengthOver = fullText.length - 255;
+  isTooLong(text, maxLength = 255) {
+    const lengthOver = text.length - maxLength;
 
     if (lengthOver > 0) {
       return lengthOver;
     } else {
       return 0;
+    }
+  },
+
+  verifyReasonLength(text, msg, maxLength = 255) {
+    const amountOver = this.isTooLong(text, maxLength);
+    if (amountOver) {
+      return msg.reply(
+        `The reason you provided is over the 255 character limit by ${amountOver}. Please shorten and retry.`
+      );
     }
   },
 };
