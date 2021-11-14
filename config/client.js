dd4a53935e0e7275176d0b4efc282b784048939a
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2021-11-14 16:43:19 -0500

    Add intents

--- config/client.js
+++ config/client.js
@@ -1,7 +1,14 @@
-const Discord = require('discord.js');
+const {Client, Intents} = require('discord.js');
 
-const client = new Discord.Client({
+const client = new Client({
   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
 });
 
 module.exports = {
