60c0459da92d62cef82a3931c684b1cc5511d56a
Michael Johansson <mikejoh12@gmail.com>
2022-02-21 21:13:38 -0600

    add embed for unhandled rejection msg

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -1,5 +1,6 @@
 const {getClient} = require('../config/client.js');
 const client = getClient();
+const {MessageEmbed} = require('discord.js');
 
 async function unhandledRejectionHandler(error) {
   try {
@@ -8,9 +9,19 @@ async function unhandledRejectionHandler(error) {
       (channel) => channel.name === 'audit-logs'
     );
     if (channel) {
-      await channel.send(
-        `UnhandledRejection error. Check logs for more info. Type: ${error.name} Message: ${error.message}`
-      );
+      const errorEmbed = new MessageEmbed()
+        .setTitle('UnhandledRejection error')
+        .setColor('RED')
+        .addFields(
+          {name: 'Name', value: error.name},
+          {name: 'Message', value: error.message},
+          {
+            name: 'Info',
+            value: `Path: ${error.path}\nMethod: ${error.method}\nHTTP Status: ${error.httpStatus}`,
+          }
+        )
+        .setTimestamp();
+      await channel.send({embeds: [errorEmbed]});
     }
   } catch (rejectionHandlerErr) {
     console.error('Error in unhandledRejection handler:', rejectionHandlerErr);
